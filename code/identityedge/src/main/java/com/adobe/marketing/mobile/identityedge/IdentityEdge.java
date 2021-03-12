174a9e0ad3a99746dfd84a5c7f7f68b75f1b85e0
Nick Porter <nporter@adobe.com>
2021-03-12 12:03:53 -0700

    Cherry pick 01c1723f2ae8716804eaf983c80416988961025f

--- code/identityedge/src/main/java/com/adobe/marketing/mobile/identityedge/IdentityEdge.java
+++ code/identityedge/src/main/java/com/adobe/marketing/mobile/identityedge/IdentityEdge.java
@@ -20,8 +20,6 @@ import com.adobe.marketing.mobile.ExtensionErrorCallback;
 import com.adobe.marketing.mobile.LoggingMode;
 import com.adobe.marketing.mobile.MobileCore;
 
-import java.util.Map;
-
 public class IdentityEdge {
     private static final String LOG_TAG = "IdentityEdge";
 
@@ -63,7 +61,7 @@ public class IdentityEdge {
         }
 
         final Event event = new Event.Builder(IdentityEdgeConstants.EventNames.IDENTITY_REQUEST_IDENTITY_ECID,
-                IdentityEdgeConstants.EventType.IDENTITY_EDGE,
+                IdentityEdgeConstants.EventType.EDGE_IDENTITY,
                 IdentityEdgeConstants.EventSource.REQUEST_IDENTITY).build();
 
         final ExtensionErrorCallback<ExtensionError> errorCallback = new ExtensionErrorCallback<ExtensionError>() {
@@ -123,7 +121,7 @@ public class IdentityEdge {
 
 
         final Event updateIdentitiesEvent = new Event.Builder(IdentityEdgeConstants.EventNames.UPDATE_IDENTITIES,
-                IdentityEdgeConstants.EventType.IDENTITY_EDGE,
+                IdentityEdgeConstants.EventType.EDGE_IDENTITY,
                 IdentityEdgeConstants.EventSource.UPDATE_IDENTITY).setEventData(identityMap.asEventData()).build();
         MobileCore.dispatchEvent(updateIdentitiesEvent, errorCallback);
     }
@@ -133,7 +131,7 @@ public class IdentityEdge {
      */
     public static void resetIdentities() {
         final Event event = new Event.Builder(IdentityEdgeConstants.EventNames.REQUEST_RESET,
-                IdentityEdgeConstants.EventType.IDENTITY_EDGE,
+                IdentityEdgeConstants.EventType.EDGE_IDENTITY,
                 IdentityEdgeConstants.EventSource.REQUEST_RESET).build();
 
         final ExtensionErrorCallback<ExtensionError> errorCallback = new ExtensionErrorCallback<ExtensionError>() {
