151fb9545f429064e6b9231d6bef4f9e3d21d9fd
Kevin Lind <lind@adobe.com>
2021-03-17 13:49:32 -0700

    Make ListenerHubSharedStateTests class public

--- code/identityedge/src/test/java/com/adobe/marketing/mobile/identityedge/ListenerHubSharedStateTests.java
+++ code/identityedge/src/test/java/com/adobe/marketing/mobile/identityedge/ListenerHubSharedStateTests.java
@@ -25,7 +25,7 @@ import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
-class ListenerHubSharedStateTests {
+public class ListenerHubSharedStateTests {
     @Mock
     private IdentityEdgeExtension mockIdentityEdgeExtension;
 
